b98c48d76c4dfb30406cdc06b889303921f7bdf0
chsintranet <35764030+chsintranet@users.noreply.github.com>
2020-10-26 09:12:02 +0000

    Update full-width-page.php
    simplified full width page template

--- full-width-page.php
+++ full-width-page.php
@@ -8,16 +8,13 @@ Template Name: Full Width Page
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<div class="col-12 text-center">
-	<div class="row mb-4">
-		<div class="col-12">
-			<span class="h3 display-5">
+<div class="row">
+	<div class="col-12">
+		<span class="h3">
 				<?php the_title(); ?>
-		    </span>
-			<hr class="small mb-4">
-			<?php the_content(); ?>
-			
-		</div>
+		</span>
+		<hr>
+		<?php the_content(); ?>
 	</div>
 </div>
 
