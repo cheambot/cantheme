8bf627ee1917aa6d98f9fc05c352d7f72d61c200
chsintranet <35764030+chsintranet@users.noreply.github.com>
2020-10-26 09:11:31 +0000

    Update cctv.php
    changed refs from cheamtheme to cantheme

--- cctv.php
+++ cctv.php
@@ -11,21 +11,21 @@ Template Name: CCTV
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<?php wp_head(); ?>
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="http://intranet/wp-content/css/bootstrap.css?v=1.8" type="text/css">
-	<link href="http://intranet/wp-content/css/fontawesome-all.css" rel="stylesheet" type="text/css">
-	<link href="http://intranet/wp-content/css/tether.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="http://staffintranet/wp-content/css/bootstrap.css?v=1.8" type="text/css">
+	<link href="http://staffintranet/wp-content/css/fontawesome-all.css" rel="stylesheet" type="text/css">
+	<link href="http://staffintranet/wp-content/css/tether.css" rel="stylesheet" type="text/css">
 	<!-- Slick CSS -->
-	<link rel="stylesheet" type="text/css" href="http://intranet/wp-content/css/slick.css"/>
-	<link rel="stylesheet" type="text/css" href="http://intranet/wp-content/css/slick-theme.css"/>
-	<link rel="stylesheet" type="text/css" href="http://intranet/wp-content/themes/cheamtheme/style.css?v=2.0.2.2"/>
-	<link href="http://intranet/wp-content/themes/cheamtheme/cctv-css/ekko-lightbox.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="http://staffintranet/wp-content/css/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="http://staffintranet/wp-content/css/slick-theme.css"/>
+	<link rel="stylesheet" type="text/css" href="http://staffintranet/wp-content/themes/cantheme/style.css?v=2.0.2.2"/>
+	<link href="http://staffintranet/wp-content/themes/cantheme/cctv-css/ekko-lightbox.css" rel="stylesheet" type="text/css">
 </head>
 <body <?php body_class(); ?>>
 	<div class="bg-dark mb-3">
 		<div class="maxwide m-auto p-0">
 			<nav class="navbar navbar-toggleable-sm navbar-light p-0 pl-2">
 				<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-				<img class="float-right" src="http://intranet/wp-content/images/small-logo.png" alt="CHS Logo">
+				<img class="float-right" src="http://staffintranet/wp-content/images/small-logo.png" alt="CHS Logo">
 			</nav>
 		</div>
 	</div>
@@ -37,7 +37,7 @@ Template Name: CCTV
 <?php
 // TEXTLINKS - THIS CODE GENERATES THE TEXT LINKS TO EACH BLOCKS ANCHOR LINK				
 // Create recursive directory iterator which skips dot folders
-$dir = new RecursiveDirectoryIterator( './wp-content/themes/cheamtheme/cctv/thumbs', FilesystemIterator::SKIP_DOTS );
+$dir = new RecursiveDirectoryIterator( './wp-content/themes/cantheme/cctv/thumbs', FilesystemIterator::SKIP_DOTS );
 // Flatten the recursive iterator, folders come before their files
 $it = new RecursiveIteratorIterator( $dir, RecursiveIteratorIterator::SELF_FIRST );
 // Maximum depth is 0 levels deeper than the base folder
@@ -68,13 +68,13 @@ foreach ( $it as $fileinfo ) {
 		<span class="h3 display-5"><?php the_title(); ?></span>
 		<hr class="small mb-4">
 		<p>Please note that these are not the live camera feeds. They are just examples of each cameras viewpoint so you can tell us which camera(s) you want footage from.</p>
-		<h5>To submit your request for <strong>Internal CCTV</strong> footage, please <a href="http://intranet/it-support/open-support-ticket">Open A Support Ticket</a></h5>
+		<h5>To submit your request for <strong>Internal CCTV</strong> footage, please <a href="http://staffintranet/it-support/open-support-ticket">Open A Support Ticket</a></h5>
 		<p>Click on the block name to jump to that buildings cameras:</p>
 		<hr class="small mb-4">
 <?php
 // TEXTLINKS - THIS CODE GENERATES THE TEXT LINKS TO EACH BLOCKS ANCHOR LINK				
 // Create recursive directory iterator which skips dot folders
-$dir = new RecursiveDirectoryIterator( './wp-content/themes/cheamtheme/cctv/thumbs', FilesystemIterator::SKIP_DOTS );
+$dir = new RecursiveDirectoryIterator( './wp-content/themes/cantheme/cctv/thumbs', FilesystemIterator::SKIP_DOTS );
 // Flatten the recursive iterator, folders come before their files
 $it = new RecursiveIteratorIterator( $dir, RecursiveIteratorIterator::SELF_FIRST );
 // Maximum depth is 0 levels deeper than the base folder
@@ -95,7 +95,7 @@ foreach ( $it as $fileinfo ) {
 <?php
 // THUMBNAILS - THIS CODE GENERATES THE THUMBNAILS FOR EACH BLOCK
 // Create recursive directory iterator which skips dot folders
-$dir = new RecursiveDirectoryIterator( './wp-content/themes/cheamtheme/cctv/thumbs', FilesystemIterator::SKIP_DOTS );
+$dir = new RecursiveDirectoryIterator( './wp-content/themes/cantheme/cctv/thumbs', FilesystemIterator::SKIP_DOTS );
 // Flatten the recursive iterator, folders come before their files
 $it = new RecursiveIteratorIterator( $dir, RecursiveIteratorIterator::SELF_FIRST );
 // Maximum depth is 1 level deeper than the base folder. The code will scan inside each
@@ -113,7 +113,7 @@ foreach ( $it as $fileinfo ) {
 		if (in_array(strtolower($filetype), $filetypes)) {
 			$file = basename( ( $fileinfo->getFilename() ), ".jpg" );
 			echo "<div class=\"col-md-2 col-sm-3 p-2 text-center\">\n";
-			echo "<a href=\"/wp-content/themes/cheamtheme/cctv/" . $dir . "/" . $file . ".jpg\"  data-toggle=\"lightbox\" data-title=\"".$file."\"  data-gallery=\"".$dir."\"><img class=\"img-fluid pb-1\" src=\"/wp-content/themes/cheamtheme/cctv/thumbs/" . $dir . "/" . $file . ".jpg\"><br>" . $file . "</a>\n";
+			echo "<a href=\"/wp-content/themes/cantheme/cctv/" . $dir . "/" . $file . ".jpg\"  data-toggle=\"lightbox\" data-title=\"".$file."\"  data-gallery=\"".$dir."\"><img class=\"img-fluid pb-1\" src=\"/wp-content/themes/cantheme/cctv/thumbs/" . $dir . "/" . $file . ".jpg\"><br>" . $file . "</a>\n";
 			echo "</div>\n";
 		}
 	}
